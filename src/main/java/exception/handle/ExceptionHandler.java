0703c594bfae51fb9a5e7e7186a4b3831e721e5d
qianjin <ggqgfj@gmail.com>
2014-01-27 11:39:26 +0800

    changed the package name

--- src/main/java/exception/handle/ExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package exception.handle;
-
-public interface ExceptionHandler {
-
-    public void handle(String contextCode, String errorCode, String errorText, Throwable t);
-
-    public void raise(String contextCode, String errorCode, String errorText);
-
-}
